66cdf4d19c508e13d47b1a5d6718e63d22f91775
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-11-29 22:12:49 +0300

    update User model: add new fields to jsonSchema

--- server/models/User.cjs
+++ server/models/User.cjs
@@ -14,10 +14,13 @@ module.exports = class User extends unique(BaseModel) {
   static get jsonSchema() {
     return {
       type: 'object',
-      required: ['email', 'password'],
+      title: 'User',
+      required: ['firstName', 'lastName', 'email', 'password'],
       properties: {
         id: { type: 'integer' },
-        email: { type: 'string', minLength: 1 },
+        firstName: { type: 'string', minLength: 1 },
+        lastName: { type: 'string', minLength: 1 },
+        email: { type: 'string', format: 'email' },
         password: { type: 'string', minLength: 3 },
       },
     };
